5777eb5a3f96e6130fd1bc1acc9dfed07a8b8f9b
Lukas Volk <lukas.volk2014@gmail.com>
2021-05-16 16:31:45 +0200

    finished dockerizing for localhosts

--- bloxz-master/src/shared/utils/api.ts
+++ bloxz-master/src/shared/utils/api.ts
@@ -1,6 +1,6 @@
 import { Action } from 'react-fetching-library'
 
-export const BASE_URL : string = process.env.BASE_URL || 'http://localhost:5001/api';
+export const BASE_URL : string = process.env.BASE_URL || 'http://localhost:8000/api';
 
 export const requestHostInterceptor = (host: string) => () => async (action: Action) => {
   return {
